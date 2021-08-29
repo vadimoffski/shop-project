629217fbbd309852ad8b98d395c088df67630b4a
vadimoffski <hryshchenko95@gmail.com>
2021-08-29 19:47:20 +0300

    added modal window

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+
+//add to cart
 let productsCountEl = document.querySelector(".products-counts");
-
 let addToCartBtns = document.querySelectorAll(".btn-add-cart");
-
 addToCartBtns.forEach((item) =>
     item.addEventListener("click", function () {
     productsCountEl.textContent = +productsCountEl.textContent+1;
   })
 );
-
+//change like btns
 let likeBtns = document.querySelectorAll(".like");
 likeBtns.forEach((item) =>
   item.addEventListener('click', function(){
   this.classList.toggle('likeClicked');
   })
 );
+//modal
+let moreDetailsBtns = document.querySelectorAll('.btn-more-details');
+let modal = document.querySelector(".modal");
+let btnClose = document.querySelector(".btn-close");
 
+moreDetailsBtns.forEach(item => item.addEventListener('click', openModal));
 
+btnClose.addEventListener('click', closeModal);
+
+modal.addEventListener("click", function(e){
+  if (e.target === modal){
+    closeModal();
+  }
+});
+function openModal(){
+  modal.classList.add('show');
+  modal.classList.remove('hide');
+}
+function closeModal(){
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+}
 
 // // change quality program
 // let decrementBtns = document.querySelectorAll(".decrement-button")[0];
